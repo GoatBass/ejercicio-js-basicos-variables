fc93ca08b45697f60719afb8b3cc6bd897887bd0
bcn-alumno <bcn-alumno@nm_madrid.local>
2020-12-03 13:41:17 +0100

    Ejercicio 3 resuelto

--- ex3.js
+++ ex3.js
@@ -11,14 +11,18 @@ let presentacion = "";
  * Pregunta 1: ¿Qué valor tengo que asignar a 'nombreCompleto' para que me muestre pantalla:
  *   * "Me llamo Mad Max?"
  *
- * 
- * 
+ *
+ *
  */
+presentacion = "Me llamo " + nombre + " " + apellido;
 
 console.log("Pregunta 1:", presentacion);
 
 /**
  * Pregunta 2: Cambia el valor de las variables adecuadamente para que, en vez de "Me llamo Mad Max", escribe "Me llamo {tu nombre} {tu apellido}"
  */
+nombre = "Eduard";
+apellido = "Garcia";
+presentacion = "Me llamo " + nombre + " " + apellido;
 
 console.log("Pregunta 2:", presentacion);
